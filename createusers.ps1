f880a1eaf3244f7c77a11ebbadbb154adccb9750
Daniel Altamirano <daltamirano78@student.lbcc.edu>
2022-04-18 05:52:50 -0700

    csv aduser script fumctional

---
+++ createusers.ps1
@@ -0,0 +1,37 @@
+
+# Import Active Directory Module
+Import-Module ActiveDirectory
+
+# Read in Secure Temporary Password (Note: Complexity)
+$Accountpassword = Read-Host -AsSecureString -Prompt "Enter Temporary Password for New Users"
+
+# Import User Data
+$csv = Import-Csv -Path C:\Users\daltamirano\Documents\adusers.csv
+
+# Loop through Users 
+foreach ($user in $csv) {
+
+    # Store User Attributes for Splatting
+    $UserInfo = @{
+        Name = $user.Name
+        DisplayName = $user.Name
+        GivenName = $user.GivenName
+        Surname = $user.Surname
+        
+        SamAccountName = $user.SamAccountName
+        Path = $user.Path
+
+        Title = $user.Title
+        Department = $user.Department
+        Company = $user.Company
+        EmailAddress = $user.EmailAddress
+        
+        AccountPassword = $Accountpassword
+        ChangePasswordAtLogon = $true    
+        Enabled = $true
+    }
+
+    # Create New Users
+    New-ADUser @UserInfo 
+    
+}
